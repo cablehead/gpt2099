9c0a4a71cced8d38aaac1f50edad0e5dcfa62065
Andy Gayton <andy@thecablelounge.com>
2024-02-14 16:51:48 -0500

    wip

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,39 +1,37 @@
 export { default as styles } from "./styles.module.scss";
 
 export const themes = {
-  "system_theme_config": {
-    "dark": "dark",
-    "light": "light",
+  system_theme_config: {
+    dark: "dark",
+    light: "light",
   },
-  "themes": {
-    "dark": {
-      "config": {
-        "icon":
-          "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtbW9vbi1zdGFyIj48cGF0aCBkPSJNMTIgM2E2IDYgMCAwIDAgOSA5IDkgOSAwIDEgMS05LTlaIi8+PHBhdGggZD0iTTE5IDN2NCIvPjxwYXRoIGQ9Ik0yMSA1aC00Ii8+PC9zdmc+",
-        "browser_theme_color": "#110000",
+  themes: {
+    dark: {
+      config: {
+        icon: "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtbW9vbi1zdGFyIj48cGF0aCBkPSJNMTIgM2E2IDYgMCAwIDAgOSA5IDkgOSAwIDEgMS05LTlaIi8+PHBhdGggZD0iTTE5IDN2NCIvPjxwYXRoIGQ9Ik0yMSA1aC00Ii8+PC9zdmc+",
+        browser_theme_color: "#110000",
       },
-      "vars": {
-        "foreground": "#E5E8EE",
-        "background": "#1D202E",
+      vars: {
+        foreground: "#E5E8EE",
+        background: "#1D202E",
         "foreground-reverse": "#1D202E",
         "background-reverse": "#E5E8EE",
-        "highlight": "#D07697",
-        "selected": "#6195FF",
+        highlight: "#D07697",
+        selected: "#6195FF",
       },
     },
-    "light": {
-      "config": {
-        "icon":
-          "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtc3VuIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSI0Ii8+PHBhdGggZD0iTTEyIDJ2MiIvPjxwYXRoIGQ9Ik0xMiAyMHYyIi8+PHBhdGggZD0ibTQuOTMgNC45MyAxLjQxIDEuNDEiLz48cGF0aCBkPSJtMTcuNjYgMTcuNjYgMS40MSAxLjQxIi8+PHBhdGggZD0iTTIgMTJoMiIvPjxwYXRoIGQ9Ik0yMCAxMmgyIi8+PHBhdGggZD0ibTYuMzQgMTcuNjYtMS40MSAxLjQxIi8+PHBhdGggZD0ibTE5LjA3IDQuOTMtMS40MSAxLjQxIi8+PC9zdmc+",
-        "browser_theme_color": "#f5f5f5",
+    light: {
+      config: {
+        icon: "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtc3VuIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSI0Ii8+PHBhdGggZD0iTTEyIDJ2MiIvPjxwYXRoIGQ9Ik0xMiAyMHYyIi8+PHBhdGggZD0ibTQuOTMgNC45MyAxLjQxIDEuNDEiLz48cGF0aCBkPSJtMTcuNjYgMTcuNjYgMS40MSAxLjQxIi8+PHBhdGggZD0iTTIgMTJoMiIvPjxwYXRoIGQ9Ik0yMCAxMmgyIi8+PHBhdGggZD0ibTYuMzQgMTcuNjYtMS40MSAxLjQxIi8+PHBhdGggZD0ibTE5LjA3IDQuOTMtMS40MSAxLjQxIi8+PC9zdmc+",
+        browser_theme_color: "#f5f5f5",
       },
-      "vars": {
-        "foreground": "#1D202E",
-        "background": "#E5E8EE",
+      vars: {
+        foreground: "#1D202E",
+        background: "#E5E8EE",
         "foreground-reverse": "#E5E8EE",
         "background-reverse": "#1D202E",
-        "highlight": "#D07697",
-        "selected": "#6195FF",
+        highlight: "#D07697",
+        selected: "#6195FF",
       },
     },
   },
