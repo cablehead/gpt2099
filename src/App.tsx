2e98e1d945a7105015d170c5e8e340a3e62298eb
Andy Gayton <andy@thecablelounge.com>
2024-02-15 15:24:15 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, For, onCleanup, Show } from "solid-js";
+import { Component, For, Match, onCleanup, Switch } from "solid-js";
 import { createStore } from "solid-js/store";
 
 import { ThemeProvider } from "solid-theme-provider";
@@ -10,35 +10,58 @@ import { matchKeyEvent } from "./util";
 import s from "./Card.module.scss";
 
 const App: Component = () => {
+  enum Mode {
+    Main = "main",
+    Add = "add",
+    Edit = "edit",
+  }
+
   const [store, setStore] = createStore({
     cards: ["Card 1", "Card 2", "Card 3"],
     focus: 0,
-    isEdit: false,
+    mode: Mode.Main,
   });
 
-  const closeEditor = (text?: string) => {
+  const endEdit = (text?: string) => {
     if (text !== undefined) {
       setStore("cards", store.focus, text);
     }
-    setStore("isEdit", false);
+    setStore("mode", Mode.Main);
+  };
+
+  const endAdd = (text?: string) => {
+    if (text !== undefined) {
+      setStore("cards", (cards) => [...cards, text]);
+      setStore("focus", store.cards.length - 1);
+    }
+    setStore("mode", Mode.Main);
   };
 
   const handleKeyPress = (event: KeyboardEvent) => {
     switch (true) {
-      // Enter - sets isEdit to true
+      // Edit
       case matchKeyEvent(event, { meta: true, code: "Enter" }):
         event.preventDefault();
-        setStore("isEdit", true);
+        setStore("mode", Mode.Edit);
         return;
-      // Right
+
+      // Add
+      case matchKeyEvent(event, { shift: true, code: "KeyA" }):
+        event.preventDefault();
+        setStore("mode", Mode.Add);
+        return;
+
+      // Nav Right
       case matchKeyEvent(event, { code: "KeyL" }):
       case matchKeyEvent(event, { code: "ArrowRight" }):
         event.preventDefault();
-        setStore("focus", (prev) =>
-          prev < store.cards.length - 1 ? prev + 1 : prev
+        setStore(
+          "focus",
+          (prev) => prev < store.cards.length - 1 ? prev + 1 : prev,
         );
         return;
-      // Left
+
+      // Nav Left
       case matchKeyEvent(event, { code: "KeyH" }):
       case matchKeyEvent(event, { code: "ArrowLeft" }):
         event.preventDefault();
@@ -71,9 +94,15 @@ const App: Component = () => {
             )}
           </For>
         </div>
-        <Show when={store.isEdit}>
-          <Editor text={store.cards[store.focus]} fin={closeEditor} />
-        </Show>
+
+        <Switch>
+          <Match when={store.mode === Mode.Edit}>
+            <Editor text={store.cards[store.focus]} fin={endEdit} />
+          </Match>
+          <Match when={store.mode === Mode.Add}>
+            <Editor text="" fin={endAdd} />
+          </Match>
+        </Switch>
       </div>
       <div style="display: flex;  align-items: center; justify-content: space-between; padding-top: 0.5em; border-top: 1px dotted var(--stp-foreground-a3);">
         <div>gpt2099</div>
