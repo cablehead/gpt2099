cbc2eaa898d559694df8db9b28a73990c8c49f44
Andy Gayton <andy@thecablelounge.com>
2024-02-14 23:22:56 -0500

    wip

---
+++ src/util.ts
@@ -0,0 +1,22 @@
+interface MatchKeyOptions {
+  shift?: boolean;
+  ctrl?: boolean;
+  alt?: boolean;
+  meta?: boolean;
+  key?: string;
+  code?: string;
+}
+
+export function matchKeyEvent(
+  event: KeyboardEvent,
+  options: MatchKeyOptions,
+): boolean {
+  return (
+    (!options.key || event.key === options.key) &&
+    (!options.code || event.code === options.code) &&
+    event.shiftKey === !!options.shift &&
+    event.ctrlKey === !!options.ctrl &&
+    event.altKey === !!options.alt &&
+    event.metaKey === !!options.meta
+  );
+}
